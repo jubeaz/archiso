d98dbc41bae505a2419b1031633776e5cea9bc6f
jubeaz <pascal.auillans@gmail.com>
2025-10-21 23:25:15 +0200

    update

--- scripts/profiledef.sh
+++ scripts/profiledef.sh
@@ -2,34 +2,35 @@
 # shellcheck disable=SC2034
 
 iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
+iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
-iso_version="$(date +%Y.%m.%d)"
+iso_application="Arch Linux Live/Rescue DVD"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux'
+           'uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/root"]="0:0:0750"
+  ["/root/.gnupg"]="0:0:700"
   ["/root/private.tgz.enc"]="0:0:0600"
   ["/root/unsecret.sh"]="0:0:0744"
   ["/root/prepare.sh"]="0:0:0744"
   ["/root/.ssh"]="0:0:0700"
   ["/root/.ssh/authorized_keys"]="0:0:0600"
   ["/var/lib/iwd"]="0:0:0700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/var/lib/iwd/wifi.psk"]="0:0:0700"
   ["/usr/local/bin/enable-ssh"]="0:0:0744"
   ["/usr/local/bin/install-base.sh"]="0:0:0744"
   ["/etc/vconsole.conf"]="0:0:0644"
 )
-
